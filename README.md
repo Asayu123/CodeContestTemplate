7e8f759d18103aec624e264ec41faff5131cac84
Asayu123 <Asayu123@users.noreply.github.com>
2017-11-07 21:53:52 +0900

    Extended an unit test to Support parameterized test.

--- README.md
+++ README.md
@@ -5,11 +5,17 @@ an Template for Code Contest.
 * Python 3.5 +
 
 ## How to Use
-* Comment out lines in input/output helper function at `src/bin/main.py` based on a problem.
+##### 1.Write a Main logic
+* Comment out lines in helper method `_input_args` and `_output_result` at `src/bin/main.py` based on a problem.
 * Write main logic at `src/bin/main.py`
-* Write test code of main logic at `src/test/test_main.py`
+##### 2.Write a Test
+* Write test parameters on method `generate_subtest_case` at `src/test/test_main.py`
+* Write test code of main logic on method `test_main` at `src/test/test_main.py`
+
+##### 3.Execute the Test
 * Then, execute unit test on project root by the following command.
     * `python -m unittest src/test/test_main.py`
+    
 * Now, You got a code for a contest.
 
 ## Tips
--- src/test/test_main.py
+++ src/test/test_main.py
@@ -10,32 +10,68 @@ from unittest.mock import MagicMock, patch
 class TestMain(unittest.TestCase):
 
     @staticmethod
-    def mock_of_inputs():  # Use this mock if a contest requires interactive input.
+    def generate_subtest_case():
+        """This function creates sub_test case for parameterized test.
+        PLEASE define INPUT and OUTPUT Value HERE for test.
+        :return: input_list, expected_list
+        :rtype list of list, list of test
+        """
+
+        input_list = [
+            ['0 3', '1', '2', '3'],
+            ['0 2', '1', '2'],
+            ['0 1', '1']
+        ]
+
+        expected_list = [
+            'ans=6',
+            'ans=3',
+            'ans=1'
+        ]
+
+        return input_list, expected_list
+
+    @staticmethod
+    def get_input_mock(inputs=None):  # Use this mock if a contest requires interactive input.
+        """Get Mock that emulate standard input. You won't use this function directly.
+
+        :param inputs: Define standard inputs that you want emulate. one element one input.
+         example: ['0 3', '1', '2', '3']
+        :type inputs: list of str
+        :return: stdin_lines
+        :rtype MagicMock
+            Caution, MagicMock raises StopIteration error when you call input function more than length of 'inputs'
+            in main.py
+        """
         stdin_lines = MagicMock()
-        stdin_lines.side_effect = ['0 3',  # you can emulate standard input lines by using this list.
-                                   '1',
-                                   '2',
-                                   '3'
-                                   ]
+        stdin_lines.side_effect = inputs  # you can emulate standard input lines by using this list.
         return stdin_lines
 
     @staticmethod
     def get_last_stdout_line(stdout):  # This method sometimes useful when you print results to stdout.
+        # If there are multiple outputs, stdout.getvalue returns combined one. so we need split to get a specific line.
         return stdout.getvalue().split('\n')[-2]
 
     def test_main(self):
         """Test Whole things, including standard Input/Output by using input mocking.
         Use this test if the contest requires input from stdin, and requires export result to stdout.
         """
-        expected = 'ans=6'
 
-        with patch(target='src.bin.main._input', new=self.mock_of_inputs()):  # emulate inputs by mock
-            with captured_stdout() as stdout:
-                main()
-                result = self.get_last_stdout_line(stdout)
+        input_list, expected_list = self.generate_subtest_case() # Get test parameters.
 
-        self.assertEqual(expected, result)
-        # if there are multiple output, stdout.getvalue returns combined one.
+        for input_lines, expected_result in zip(input_list, expected_list):  # extract params for sub_test.
+            # Execute sub_test for each params.
+            with self.subTest(input=input_lines, output=expected_result):
+
+                # Emulate input_lines by mock
+                with patch(target='src.bin.main._input', new=self.get_input_mock(inputs=input_lines)):
+
+                    # Execute main logic with capturing standard output.
+                    with captured_stdout() as stdout:
+                        main()
+                        actual_result = self.get_last_stdout_line(stdout)
+
+                        self.assertEqual(expected_result, actual_result)
 
     @skip
     def test__main(self):
